94779caeb11f90d3913c4c5d350c6bd0adfd60c8
Avinash Kumar <avinashkumar532004@gmail.com>
2023-02-13 17:35:21 +0530

    fix scroll effect

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,21 +8,16 @@ import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 const HomePage = () => {
-  const scrollRef = useRef(null);
-  useEffect(() => {
-    scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-  }, [scrollRef]);
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   if (!user) navigate("/EnterName");
   return (
     <div className="homePage">
-      <Navbar />
+      <Navbar className="navContainer" />
       <ChatSection />
       <div className="homeFooter">
         <ChatInput />
       </div>
-      <div ref={scrollRef}></div>
     </div>
   );
 };
