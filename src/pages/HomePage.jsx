0cfc37b5863bd533013fa068637eff3d518428b2
Avinash Kumar <avinashkumar532004@gmail.com>
2023-02-13 07:21:34 +0530

    change css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,9 @@ import ChatInput from "../components/ChatInput";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 const HomePage = () => {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  if(!user) navigate("/EnterName")
+  if (!user) navigate("/EnterName");
   return (
     <div className="homePage">
       <Navbar />
